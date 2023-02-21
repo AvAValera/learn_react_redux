c22fcb1d9f1745ed1cdcd8af155ce5be7f619a2b
AvAValera <avavarenik07@gmail.com>
2023-02-21 14:22:41 +0200

    init new branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import Header from "./components/Header";
-import Todo from "./components/todo/Todo";
-import { selectTheme } from "./store/theme";
-import { useSelector } from "react-redux";
+
 
 function App() {
-    const theme = useSelector(selectTheme);
 
 
     return (
-        <div className={`app ${theme} relative w-full h-screen min-w-[320px]`}>
-            <Header />
-            <Todo />
+        <div >
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./style.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import store from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
+        <Provider >
             <App />
         </Provider>
     </React.StrictMode>
